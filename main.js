fff297786fe282dc213925ecc88f524a89ff8b27
Alexandre Cochet <alexcochet1@hotmail.fr>
2019-03-20 10:34:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Alex'));
+console.log(chalk.yellow('Mika'));
+console.log(chalk.red('Valentin'));
+console.log(chalk.green('Remy'));
+console.log(chalk.cyan('Jeremy'));
+
+
+
